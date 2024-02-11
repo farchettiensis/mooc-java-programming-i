c9b14eb2965d57cf7646126c41da5a97c912b4d9
farchettiensis <farchettiensis@users.noreply.github.com>
2024-02-10 22:38:05 -0300

    Repository initialization

---
+++ part01-Part01_30.LargerThanOrEqualTo/src/main/java/LargerThanOrEqualTo.java
@@ -0,0 +1,24 @@
+
+import java.util.Scanner;
+
+public class LargerThanOrEqualTo {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Give the first number:");
+        int first = scanner.nextInt();
+
+        System.out.println("Give the second number:");
+        int second = scanner.nextInt();
+
+        if (first > second) {
+            System.out.println("Greater number is: " + first);
+        } else if (second > first) {
+            System.out.println("Greater number is: " + second);
+        } else {
+            System.out.println("The numbers are equal!");
+        }
+
+    }
+}
---
+++ part01-Part01_36.LeapYear/src/main/java/LeapYear.java
@@ -0,0 +1,31 @@
+
+import java.util.Scanner;
+
+public class LeapYear {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Give a year:");
+        int year = scanner.nextInt();
+
+        boolean isLeapYear = false;
+
+        if (year % 4 == 0) {
+            if (year % 100 == 0) {
+                if (year % 400 == 0) {
+                    isLeapYear = true;
+                }
+            } else {
+                isLeapYear = true;
+            }
+        }
+        
+        if (isLeapYear) {
+            System.out.println("The year is a leap year.");
+        } else {
+            System.out.println("The year is not a leap year.");
+        }
+
+    }
+}
