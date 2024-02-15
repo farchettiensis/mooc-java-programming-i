25453c5837071b29e0f5e86417eb0279edda307a
farchettiensis <farchettiensis@users.noreply.github.com>
2024-02-14 21:47:32 -0300

    part02-Part02_23-Part02_26

--- part02-Part02_26.DivisibleByThree/src/main/java/DivisibleByThree.java
+++ part02-Part02_26.DivisibleByThree/src/main/java/DivisibleByThree.java
@@ -6,6 +6,23 @@ public class DivisibleByThree {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Start: ");
+        int start = scanner.nextInt();
+
+        System.out.println("End: ");
+        int end = scanner.nextInt();
+
+        divisibleByThreeInRange(start, end);
+
+    }
+
+    public static void divisibleByThreeInRange(int beginning, int end) {
+        for (beginning = beginning; beginning <= end; beginning++) {
+            if (beginning % 3 == 0) {
+                System.out.println(beginning);
+            }
+        }
+
     }
 
 }
