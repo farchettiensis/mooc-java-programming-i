c35864abe213713c66e58420956aff67a1f232ac
farchettiensis <farchettiensis@users.noreply.github.com>
2024-02-15 21:57:24 -0300

    part02-Part02_27-Part02_32

--- part02-Part02_30.Smallest/src/main/java/Smallest.java
+++ part02-Part02_30.Smallest/src/main/java/Smallest.java
@@ -2,10 +2,8 @@
 public class Smallest {
 
     public static int smallest(int number1, int number2) {
-        // Write some code here
-        // don't print anything inside this method
-        // there must be a return in the end of the method
-        return -1;
+        int smallest = (number1 < number2) ? number1 : number2;
+        return smallest;
     }
 
     public static void main(String[] args) {
