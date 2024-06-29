300284ae607013c02ece5e308314df86517870ec
farchettiensis <farchettiensis@users.noreply.github.com>
2024-06-29 01:32:36 -0300

    Part03_32

--- part03-Part03_32.PersonalDetails/src/main/java/PersonalDetails.java
+++ part03-Part03_32.PersonalDetails/src/main/java/PersonalDetails.java
@@ -6,7 +6,24 @@ public class PersonalDetails {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int sum = 0;
+        int count = 0;
+        int length = 0;
+        String longest = "";
+        
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("")) break;
+            
+            String[] parts = input.split(",");
+            if (parts[0].length() > longest.length()) {
+                longest = parts[0];
+            }
+            count++;
+            sum += Integer.valueOf(parts[1]);
+        }
 
-
+        System.out.println("Longest name: " + longest);
+        System.out.println("Average of the birth years: " + (1.0 * sum / count));
     }
 }
