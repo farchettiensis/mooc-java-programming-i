d40ffc719f0c70bdfe3bd43ef23cc5e23a052826
farchettiensis <farchettiensis@users.noreply.github.com>
2024-06-30 00:07:12 -0300

    part04-Part04_01-04_02

--- part04-Part04_01.YourFirstAccount/src/main/java/YourFirstAccount.java
+++ part04-Part04_01.YourFirstAccount/src/main/java/YourFirstAccount.java
@@ -2,7 +2,10 @@
 public class YourFirstAccount {
 
     public static void main(String[] args) {
-        // Do not touch the code in Account.java
-        // Write your program here
+        Account theHuttAccount = new Account("Dr. Jamil's account", 100.0);
+        
+        theHuttAccount.deposit(20);
+        
+        System.out.println(theHuttAccount);
     }
 }
