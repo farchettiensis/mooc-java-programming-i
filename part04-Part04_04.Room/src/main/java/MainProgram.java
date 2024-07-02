85f52b84c77ddb6cd490fae3e6554c888864b282
farchettiensis <farchettiensis@users.noreply.github.com>
2024-07-02 00:09:23 -0300

    part04-Part04_05

--- part04-Part04_04.Room/src/main/java/MainProgram.java
+++ part04-Part04_04.Room/src/main/java/MainProgram.java
@@ -5,9 +5,5 @@ public class MainProgram {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-        // Follow the instructions carefully 
-        // and run the tests.
     }
-
 }
--- part04-Part04_05.Whistle/src/main/java/Main.java
+++ part04-Part04_05.Whistle/src/main/java/Main.java
@@ -2,14 +2,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        // This is just an empty main method you can use to test
-        // the Whistle class. Try for example:
-
-//        Whistle duckWhistle = new Whistle("Kvaak");
-//        Whistle roosterWhistle = new Whistle("Peef");
-//
-//        duckWhistle.sound();
-//        roosterWhistle.sound();
-//        duckWhistle.sound();
+        Whistle catWhistle = new Whistle("pspspsps");
+        catWhistle.sound();
     }
 }
