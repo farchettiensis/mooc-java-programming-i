7b3ba519b9b2449fa62edcaea13f79be3e377ece
farchettiensis <farchettiensis@users.noreply.github.com>
2024-07-09 16:45:16 -0300

    Part04_13-04_14

---
+++ part04-Part04_14.Multiplier/src/main/java/Multiplier.java
@@ -0,0 +1,16 @@
+
+/**
+ *
+ * @author fernando.archetti_li
+ */
+public class Multiplier {
+    private int number;
+
+    public Multiplier(int number) {
+        this.number = number;
+    }
+    
+    public int multiply(int number) {
+        return this.number * number;
+    }
+}
