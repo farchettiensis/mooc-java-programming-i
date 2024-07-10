dd3061c88322c00faaebf1a85f55253f1d67675e
farchettiensis <farchettiensis@users.noreply.github.com>
2024-07-10 15:35:12 -0300

    Part04_15.NumberStatistics

--- part04-Part04_15.NumberStatistics/src/main/java/MainProgram.java
+++ part04-Part04_15.NumberStatistics/src/main/java/MainProgram.java
@@ -1,16 +1,45 @@
 
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class MainProgram {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        // you can write test code here
-        // however, remove all unnecessary code when doing the final parts of the exercise
+        ArrayList<Integer> numbers = new ArrayList<>();
+        System.out.println("Enter numbers:");
 
-        // In order for the tests to work, the objects must be created in the
-        // correct order in the main program. First the object that tracks the total
-        // sum, secondly the object that tracks the sum of even numbers, 
-        // and lastly the one that tracks the sum of odd numbers!
+        int number;
+        do {
+            number = scanner.nextInt();
+            if (number != -1) {
+                numbers.add(number);
+            }
+        } while (number != -1);
+
+        Statistics allStats = new Statistics();
+        Statistics evenStats = new Statistics();
+        Statistics oddStats = new Statistics();
+        for (int num : numbers) {
+            allStats.addNumber(num);
+
+            if (num % 2 == 0) {
+                evenStats.addNumber(num);
+            } else {
+                oddStats.addNumber(num);
+            }
+        }
+
+//        Statistics statistics = new Statistics();
+//        statistics.addNumber(3);
+//        statistics.addNumber(5);
+//        statistics.addNumber(1);
+//        statistics.addNumber(2);
+//        System.out.println("Count: " + statistics.getCount());
+//        System.out.println("Sum: " + statistics.sum());
+//        System.out.println("Average: " + statistics.average());
+        System.out.println("Sum: " + allStats.sum());
+        System.out.println("Sum of even numbers: " + evenStats.sum());
+        System.out.println("Sum of odd numbers: " + oddStats.sum());
     }
 }
