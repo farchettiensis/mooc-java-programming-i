1bdee67f6e9a81dc9ce8dfaa4fdbc9bace3d43db
farchettiensis <farchettiensis@users.noreply.github.com>
2024-07-12 17:01:24 -0300

    part04-Part04_29-04_30

--- part04-Part04_29.RecordsFromAFile/src/main/java/RecordsFromAFile.java
+++ part04-Part04_29.RecordsFromAFile/src/main/java/RecordsFromAFile.java
@@ -6,6 +6,22 @@ public class RecordsFromAFile {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Name of the file: ");
+        String fileName = scanner.nextLine();
 
+        try (Scanner fileReader = new Scanner(Paths.get(fileName))) {
+            while (fileReader.hasNextLine()) {
+                String line = fileReader.nextLine();
+                String[] parts = line.split(",");
+                String name = parts[0];
+                int age = Integer.parseInt(parts[1]);
+
+                String yearWord = (age == 1) ? "year" : "years";
+
+                System.out.println(name + ", age: " + age + " " + yearWord);
+            }
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
     }
 }
--- part04-Part04_30.StoringRecords/src/main/java/StoringRecords.java
+++ part04-Part04_30.StoringRecords/src/main/java/StoringRecords.java
@@ -6,25 +6,34 @@ import java.util.Scanner;
 public class StoringRecords {
 
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
         System.out.println("Filename:");
-        String file = scan.nextLine();
+        String fileName = scanner.nextLine();
 
-        ArrayList<Person> records = readRecordsFromFile(file);
+        ArrayList<Person> records = readRecordsFromFile(fileName);
         System.out.println("Persons: " + records.size());
         System.out.println("Persons:");
         for (Person person : records) {
             System.out.println(person);
-
         }
     }
 
-    public static ArrayList<Person> readRecordsFromFile(String file) {
+    public static ArrayList<Person> readRecordsFromFile(String fileName) {
         ArrayList<Person> persons = new ArrayList<>();
 
-        // Write here the code for reading from file
-        // and printing the read records
+        try (Scanner fileReader = new Scanner(Paths.get(fileName))) {
+            while (fileReader.hasNextLine()) {
+                String row = fileReader.nextLine();
+                String[] parts = row.split(",");
+                String name = parts[0];
+                int age = Integer.parseInt(parts[1]);
+                persons.add(new Person(name, age));
+            }
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+
         return persons;
 
     }
